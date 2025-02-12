d2bd5c032f7f55ef5123fc8d0c3679137f1ae352
tkhs-0114 <tkhs.mail0114@gmail.com>
2025-02-12 13:50:28 +0000

    httpsでの開発とホットリロードの設定

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, context) => {
+        config.watchOptions = {
+            poll: 300,
+            aggregateTimeout: 300
+        }
+        return config
+    }
+}
 
 export default nextConfig;
